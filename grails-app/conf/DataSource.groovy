3aa10a2d4ed2440b794155a963f96fa565e6886f
luz <luzma_87@yahoo.com>
2013-12-17 10:48:39 -0500

    arreglado bug en formula polinomica

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -14,7 +14,7 @@ environments {
         dataSource {
             dbCreate = "update"
 //            url = "jdbc:postgresql://127.0.0.1:5432/ofrt"
-            url = "jdbc:postgresql://10.0.0.2:5432/ofrt"
+            url = "jdbc:postgresql://10.0.0.2:5432/oferentes"
             username = "postgres"
             password = "postgres"
         }
